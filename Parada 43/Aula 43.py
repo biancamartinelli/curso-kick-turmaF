97525833bd81eedbac8655d4709d1174ca418f86
Bianca Martinelli <bianca.amartinelli@gmail.com>
2023-07-31 19:20:03 -0300

    aula 1

---
+++ Parada 43/Aula 43.py
@@ -0,0 +1,26 @@
+"""_summary_
+"""
+
+nome = "Bianca"
+sexo = "F"
+idade = 27
+altura = 1.58
+
+print(nome, sexo, idade, altura)
+
+print("Hello world")
+
+dia = "Domingo"
+print(dia)
+
+dd = 10
+mm = 12
+aa = 2023
+print("Dia: ",dd, "Mês: ",mm, "Ano: ",aa)
+
+
+nome = input("Qual o seu nome? ")
+idade = input("Qual a sua idade? ")
+
+print(nome)
+print(idade)
