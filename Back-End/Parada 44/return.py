46556a75fc9a3d49369c9bdc880d9de2ae7a7c09
Bianca Martinelli <bianca.amartinelli@gmail.com>
2023-08-08 19:59:28 -0300

    parada 44

---
+++ Back-End/Parada 44/return.py
@@ -0,0 +1,19 @@
+def soma(n1, n2):
+    soma = n1 + n2
+    return soma
+
+resultado = soma(1, 2)
+
+print(resultado)
+
+
+def par_impar(num):
+    resultado = num % 2
+    
+    if resultado == 0:
+        return True
+    else:
+        return False
+    
+    print(par(3))
+     
